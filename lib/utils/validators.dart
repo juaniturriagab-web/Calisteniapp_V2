68c220fc6eb461632be69e1257d24db02ee29a5e
Ichi6o05 <diegoacevedomazuela@gmail.com>
2025-12-06 00:37:14 -0300

    Eliminacion algunas validaciones

--- lib/utils/validators.dart
+++ lib/utils/validators.dart
@@ -6,24 +6,17 @@ class Validators {
   }
 
   static String? validatePassword(String pass) {
-    if (pass.length < 12) return 'Mínimo 12 caracteres';
-    final hasUpper = RegExp(r'[A-Z]').hasMatch(pass);
-    final hasLower = RegExp(r'[a-z]').hasMatch(pass);
-    final hasNumber = RegExp(r'[0-9]').hasMatch(pass);
-    final hasSymbol = RegExp(r'[!@#\$%^&*(),.?":{}|<>_\-]').hasMatch(pass);
-
-    if (!hasUpper || !hasLower || !hasNumber || !hasSymbol) {
-      return 'Incluye mayúsculas, minúsculas, números y símbolos';
+    if (pass.length < 12) {
+    return  'Contraseña poco segura';
     }
-
     return null;
   }
 
   static String? validatePhone(String phone) {
     final txt = phone.trim();
-    if (!txt.startsWith('+')) return 'Incluye código de país. Ej: +52XXXXXXXXXX';
+    if (!txt.startsWith('+')) return 'Incluye código de país. Ej: +56XXXXXXXXX';
     final digits = txt.substring(1);
-    if (!RegExp(r'^[0-9]+$').hasMatch(digits)) return 'Solo números después de +';
+    if (!RegExp(r'^[0-9]+$').hasMatch(digits)) return 'Solo números después de +, elimine espacios u otros caracteres';
     if (digits.length < 10 || digits.length > 15) return 'Número inválido';
     return null;
   }
